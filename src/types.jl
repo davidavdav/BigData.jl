bd38f96b2fe47dc5cec0a5db3ae57400afc69ae5
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-10-21 15:58:40 +0200

    Make dataormatrix depend on type

--- src/types.jl
+++ src/types.jl
@@ -1,11 +1,17 @@
+## types.jl
+## (c) 2014 Davod A. van Leewen
+
 ## A data handle, either in memory or on disk, perhaps even mmapped but I haven't seen any 
 ## advantage of that.  It contains a list of either files (where the data is stored)
 ## or data units.  The point is, that in processing, these units can naturally be processed
 ## independently.  
+
+## The API is a dictionary of functions that help loading the data into memory
+## Compulsory is: :load, useful is: size
 type Data
     datatype::Type
     list::Vector
-    read::Union(Function,Nothing)
+    API::Dict
 end
 
-typealias DataOrMatrix Union(Data, Matrix)
+typealias DataOrMatrix{T} Union(Data, Matrix{T})
